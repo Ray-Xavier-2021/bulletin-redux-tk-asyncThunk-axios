cfb41012f9aafdc2bdc801cfe2b0b539570fd109
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-10-04 19:55:19 -0400

    modified postSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,35 +1,30 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 // Import sub from date-fns to set initial state of date subtract the post time in object
 import { sub } from "date-fns";
 
-const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: 'Really fun to learn.',
-        date: sub(new Date(), {minutes: 10}).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            heart: 0,
-            coffee: 0,
-            wow: 0,
-            rocket: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: 'If only there was pizza.',
-        date: sub(new Date(), {minutes: 5}).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            heart: 0,
-            coffee: 0,
-            wow: 0,
-            rocket: 0
-        }
-    }
-]
+import axios from "axios";
+
+// Create variable to hold dummy API data
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+// Removed static state
+const initialState = {
+    posts: [],
+    status: 'idle', // 'loading' || 'succeeded' || 'failed'
+    error: null
+}
+
+// Create a fetch/ read posts asyncThunk function that gets a post
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get(POSTS_URL)
+    return response.data
+})
+
+// Create a post/ create post asyncThunk function that adds a post
+export const addNewPost = createAsyncThunk('posts/addNewPosts', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
+    return response.dsta
+})
 
 const postsSlice = createSlice({
     name: 'posts',
@@ -37,7 +32,7 @@ const postsSlice = createSlice({
     reducers: {
         addPost: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             // Generate / format object data and return payload using prepare callback
             prepare(title, content, userId) {
@@ -64,7 +59,7 @@ const postsSlice = createSlice({
         // Set a reaction added reducer that changes the stat of the reaction button
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             // Check if post exists the give reaction button functionality
             if (existingPost) {
                 existingPost.reactions[reaction]++
@@ -73,7 +68,7 @@ const postsSlice = createSlice({
     }
 })
 
-export const allSelectedPosts = (state) => state.posts
+export const allSelectedPosts = (state) => state.posts.posts
 
 export const { addPost, reactionAdded } = postsSlice.actions
 
